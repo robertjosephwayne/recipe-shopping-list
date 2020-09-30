630ecd28f6a9df50922878d1b0b9f2ff82d616aa
Robert <robertjwayne@gmail.com>
2020-09-30 07:30:23 +0700

    Create RecipeItem component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
+    RecipeItemComponent,
     ShoppingListComponent
   ],
   imports: [
