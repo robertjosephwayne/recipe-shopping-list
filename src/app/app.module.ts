43f6ab05819012e1789f10bb662653cedfb9d36d
Robert <robertjwayne@gmail.com>
2020-09-30 07:06:00 +0700

    Create RecipeList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipeListComponent,
     ShoppingListComponent
   ],
   imports: [
