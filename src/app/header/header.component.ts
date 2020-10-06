97cfdf1b1266f0156bb29dc0752861a05e59a235
Robert <robertjwayne@gmail.com>
2020-10-06 14:14:37 +0700

    Update header navigation links to use routerLink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature: string): void {
-    this.featureSelected.emit(feature);
-  }
-
+export class HeaderComponent {
 }
