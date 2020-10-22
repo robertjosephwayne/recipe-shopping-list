d4b6a04a4367359a53f8c87f44675d8844bca45e
Robert <robertjwayne@gmail.com>
2020-10-22 16:06:00 +0700

    Create logging service
    This service will have two different instances because the shopping list module is loaded lazily.

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { LoggingService } from '../logging.service';
 import { SharedModule } from '../shared/shared.module';
 
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
@@ -17,6 +18,9 @@ import { ShoppingListComponent } from './shopping-list.component';
     RouterModule,
     SharedModule,
     FormsModule
+  ],
+  providers: [
+    LoggingService
   ]
 })
 export class ShoppingListModule {
