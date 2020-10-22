57decdc26cf9261f80137498c11d6fc3167e03f9
Robert <robertjwayne@gmail.com>
2020-10-22 13:29:50 +0700

    Move recipes routing into recipes module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesComponent } from './recipes.component';
 
 @NgModule({
@@ -20,6 +21,7 @@ import { RecipesComponent } from './recipes.component';
     RecipeEditComponent,
   ],
   imports: [
+    RecipesRoutingModule,
     RouterModule,
     CommonModule,
     ReactiveFormsModule
