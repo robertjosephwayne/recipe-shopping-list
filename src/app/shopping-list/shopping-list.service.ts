534e54f8e310e93c0d43b52b20a57434d8d354bf
Robert <robertjwayne@gmail.com>
2020-10-25 15:20:45 +0700

    Create shopping list reducer

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,10 +5,6 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
   startedEditing = new Subject<number>();
-  private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
 
   getIngredients(): Ingredient[] {
     return this.ingredients.slice();
